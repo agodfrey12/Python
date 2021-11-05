b5dce28fc9ad3d16e44cdd8a646a007752fb1ae4
Aaron Godfrey <lolp1neco@gmail.com>
2021-11-05 18:35:03 -0500

    data and code from lab

---
+++ CircuitPython/servo_calibration.py
@@ -0,0 +1,19 @@
+import time
+import board
+import pulseio
+
+# Initialize PWM output for the servo (on pin A2):
+servo = pulseio.PWMOut(board.A2, frequency=50)
+
+# Create a function to simplify setting PWM duty cycle for the servo:
+def servo_duty_cycle(pulse_ms, frequency=50):
+    period_ms = 1.0 / frequency * 1000.0
+    duty_cycle = int(pulse_ms / (period_ms / 65535.0))
+    return duty_cycle
+
+# Main loop will run forever moving between 1.0 and 2.0 mS long pulses:
+while True:
+    pulse_ms = 0.65
+    print('Milli Second Pulse = ',pulse_ms)
+    servo.duty_cycle = servo_duty_cycle(pulse_ms)
+    time.sleep(1.0)
