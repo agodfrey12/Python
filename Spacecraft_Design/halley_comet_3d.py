35b2ad366da64d329cd49c2cfd2a3c110995d5bf
Aaron Godfrey <lolp1neco@gmail.com>
2021-11-14 20:23:55 -0600

    halley orbit in 3d

--- Spacecraft_Design/halley_comet_3d.py
+++ Spacecraft_Design/halley_comet_3d.py
@@ -4,13 +4,13 @@ from mpl_toolkits.mplot3d import Axes3D
 
 ##Sun Parameters
 G = 6.6742*10**-11; #Gravitational constant
-Msun = 1.989*10**30 
+Msun = 1.989*10**30 #mass of sun
 musun = G*Msun
 Rsun = 696347055.0 #meters
 
 ##True Anamoly
 nu = np.linspace(0,2*np.pi,1000)
-print(nu)
+
 ##Semi Major Axis
 a = 17.94*1.496e11
 
@@ -18,13 +18,13 @@ a = 17.94*1.496e11
 e = 0.967
 
 ##inclination
-i = 162.2
+i = 162.2*np.pi/180.0
 
 ##Longitude of the Ascending Node
-W = 57.987
+W = 57.987*np.pi/180.0
 
 #Argument of the periaps
-w = 110.9135
+w = 110.9135*np.pi/180.0
 
 ##plot in the orbital plane
 ###Phat and Qhat
@@ -34,13 +34,15 @@ xp = r*np.cos(nu)
 yq = r*np.sin(nu)
 
 plt.plot(xp,yq,'r-')
-plt.plot(xp[0],yq[0],'r*',markerSize=20)
+plt.plot(xp[0],yq[0],'r*',markersize=15)
 theta = np.linspace(0,2*np.pi,1000)
 xsun = Rsun*np.cos(theta)
 ysun = Rsun*np.sin(theta)
 plt.plot(xsun,ysun,'b-')
 plt.axis('equal')
 plt.title('Orbital Plane')
+plt.xlabel('X-axis (m)')
+plt.ylabel('Y-axis (m)')
 
 ###Rotate to Sun Centered Inertial Frame (KCI)
 zr = 0*xp
@@ -60,7 +62,11 @@ for x in range(0,len(xp)):
   zk[x] = xyzi[2]
   
 fig = plt.figure()
-x = fig.add_subplot(111, projection='3d')
+ax = fig.add_subplot(111, projection='3d')
+ax.set_title('3d Orbit of Halley Comet')
+ax.set_xlabel('X-axis (m)')
+ax.set_ylabel('Y-axis (m)')
+ax.set_zlabel('Z-axis (m)')
 ax.plot(xi,yj,zk,'r-')
 ax.scatter(xi[0],yj[0],zk[0],'r*',s=20)
 u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
