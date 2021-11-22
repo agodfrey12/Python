87bae4d5e650f1b2b1e240b0b2c0d3c2d47287e0
Aaron Godfrey <lolp1neco@gmail.com>
2021-11-22 17:19:26 -0600

    code for a bluetooth data connection

---
+++ CircuitPython/Instrumentation Project/doink_w_bluetooth.py
@@ -0,0 +1,61 @@
+### DOINK code ###
+
+### notes for the future
+## make it write to a txt file on board
+## use switch to start 
+## switch other way to stop
+
+# make sure file is named code.py
+
+import math
+import board
+import time
+import busio
+import digitalio
+import adafruit_lis3mdl # magnetometer
+from adafruit_lsm6ds.lsm6ds33 import LSM6DS33 # accel and rate gyro
+from adafruit_ble import BLERadio
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.services.nordic import UARTService
+
+#print(dir(adafruit_lis3mdl))
+#print(dir(LSM6DS33))
+
+#Setup blue tooth
+ble = BLERadio()
+uart_server = UARTService()
+advertisement = ProvideServicesAdvertisement(uart_server)
+print('Bluetooth Setup')
+
+#start time
+start_time = time.monotonic()
+
+#I2C setup
+i2c = board.I2C()                           # tells the CPB we are using I2C
+sensor1 = LSM6DS33(i2c)                     # sens 1 is the accel/gyro on I2C
+sensor2 = adafruit_lis3mdl.LIS3MDL(i2c)     # sens 2 is the magnetometer on I2C
+
+#make this the switch
+#buttonA = digitalio.DigitalInOut(board.D4)
+#buttonA.direction = digitalio.Direction.INPUT
+#buttonA.pull = digitalio.Pull.DOWN
+
+while True:
+    print('Not connected')
+    ble.start_advertising(advertisement)
+    while not ble.connected:
+        pass
+    print('Connected')
+    ble.stop_advertising()
+    
+    while ble.connected:
+        while time.monotonic() - start_time < 10:
+            t = time.monotonic() - start_time
+            ax,ay,az = sensor1.acceleration         # acceleration data in 3 axis
+            wx,wy,wz = sensor1.gyro                 # rate gyro data in 3 axis
+            bx,by,bz = sensor2.magnetic             # magnetometer data in 3 axis
+            #heading = math.atan2(by,bx)*180/math.pi
+            #print(heading)
+            print(t,ax,ay,az,wx,wy,wz,bx,by,bz)
+            time.sleep(0.05)
+            uart_server.write('{}{}{}{}{}{}{}{}{}{}\n'.format(t,ax,ay,az,wx,wy,wz,bx,by,bz))
